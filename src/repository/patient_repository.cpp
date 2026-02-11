0a28a41b4e8ea21320a26dd85ff68ca894ae75a0
Kilian <kischoenhaar@gmail.com>
2026-02-11 19:13:01 +0100

    add patient_repository files

---
+++ src/repository/patient_repository.cpp
@@ -0,0 +1,9 @@
+/*
+ * File: patient_repository
+ * Author: Kilian | ItwenzyI
+ * Created: 07.02.2026
+ * Description: 
+ */
+//
+
+#include "patient_repository.hpp"
---
+++ src/repository/patient_repository.hpp
@@ -0,0 +1,16 @@
+/*
+ * File: patient_repository
+ * Author: Kilian | ItwenzyI
+ * Created: 07.02.2026
+ * Description: 
+ */
+//
+
+#ifndef MEDICATION_PLAN_PATIENT_REPOSITORY_HPP
+#define MEDICATION_PLAN_PATIENT_REPOSITORY_HPP
+
+class PatientRepository {
+
+};
+
+#endif //MEDICATION_PLAN_PATIENT_REPOSITORY_HPP
