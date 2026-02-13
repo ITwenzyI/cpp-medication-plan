49354f0b7c4eb7300fbfcf00a00097362f1de313
Kilian <kischoenhaar@gmail.com>
2026-02-13 18:43:28 +0100

    add getAllPatients function definition for PatientRepository class: - prepare Statement - for every row maps to Patient struct and push_back in vector - returns vector with all patients

--- src/repository/patient_repository.cpp
+++ src/repository/patient_repository.cpp
@@ -44,4 +44,28 @@ Patient PatientRepository::createPatient(const Patient& p) {
 }
 
 std::vector<Patient> PatientRepository::getAllPatients() {
+    auto stmt =
+        db_.prepare("SELECT id, name, birth_date, nationality FROM patients ORDER BY id ASC;");
+
+    std::vector<Patient> result;
+
+    while (true) {
+        int rc = stmt.step();
+
+        if (rc == SQLITE_ROW) {
+            Patient temp;
+            temp.id = stmt.getInt(0);
+            temp.name = stmt.getText(1);
+            temp.birth_date = stmt.getText(2);
+            temp.nationality = stmt.getText(3);
+            result.push_back(temp);
+            continue;
+        } else if (rc == SQLITE_DONE) {
+            break;
+        } else {
+            throw std::runtime_error("STEP fehlgeschlagen.");
+        }
+    }
+
+    return result;
 }
