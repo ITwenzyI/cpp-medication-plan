afc325b093b6c07419623780ed505e93f1e6ee95
Kilian <kischoenhaar@gmail.com>
2026-02-25 19:21:33 +0100

    feat(validation): add birth_date validation function
    - use string_view
    - checks isEmptyOrBlank
    - checks size == 10
    - checks '-' for YYYY-MM-DD
    - checks if YYYY MM DD isdigit
    - parse year, month, day with std::from_chars
    - checks year >= 1900 && year <= 2027
    - checks month < 1 || month > 12
    - checks day < 1 || day > 31

---
+++ src/common/validation/birth_date_validation.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "common/validation/string_validation.hpp"
+#include <cctype>
+#include <charconv>
+#include <string_view>
+
+namespace common::validation {
+
+// Format: YYYY-MM-DD
+bool isValidBirthDate(std::string_view sv) {
+    if (isEmptyOrBlank(sv)) {
+        return false;
+    }
+    if (sv.size() != 10) {
+        return false;
+    }
+    if (sv[4] != '-' || sv[7] != '-') {
+        return false;
+    }
+    for (size_t i = 0; i < sv.size(); i++) {
+        if (i == 4 || i == 7) {
+            continue;
+        }
+        if (!std::isdigit(static_cast<unsigned char>(sv[i]))) {
+            return false;
+        }
+    }
+
+    auto year_sv = sv.substr(0, 4);
+    auto month_sv = sv.substr(5, 2);
+    auto day_sv = sv.substr(8, 2);
+    int year = 0, month = 0, day = 0;
+
+    auto res_year = std::from_chars(year_sv.data(), year_sv.data() + year_sv.size(), year);
+    if (res_year.ec != std::errc{} || res_year.ptr != (year_sv.data() + year_sv.size())) {
+        return false;
+    }
+    auto res_month = std::from_chars(month_sv.data(), month_sv.data() + month_sv.size(), month);
+    if (res_month.ec != std::errc{} || res_month.ptr != (month_sv.data() + month_sv.size())) {
+        return false;
+    }
+    auto res_day = std::from_chars(day_sv.data(), day_sv.data() + day_sv.size(), day);
+    if (res_day.ec != std::errc{} || res_day.ptr != (day_sv.data() + day_sv.size())) {
+        return false;
+    }
+
+    if (year >= 1900 && year <= 2027) {
+        return false;
+    }
+
+    if (month < 1 || month > 12) {
+        return false;
+    }
+
+    if (day < 1 || day > 31) {
+        return false;
+    }
+
+    return true;
+}
+} // namespace common::validation
