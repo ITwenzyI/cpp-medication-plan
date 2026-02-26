e8511d0901fda314d603b34050ced5b4c6e5354b
Kilian <kischoenhaar@gmail.com>
2026-02-26 12:50:31 +0100

    feat(infrastructure::persistenc): add Medication header

---
+++ src/infrastructure/persistence/sqlite/medication_repository_sqlite.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "application/ports/medication_repository.hpp"
+#include "common/result/result.hpp"
+#include "infrastructure/db/database.hpp"
+#include <string_view>
+#include <vector>
+
+namespace infrastructure::persistence::sqlite {
+class MedicationRepositorySqlite : public application::ports::IMedicationRepository {
+  public:
+    explicit MedicationRepositorySqlite(infrastructure::db::Database& db);
+
+    [[nodiscard]] common::result::Result<domain::Medication> createMedication(
+        const domain::Medication& m) override;
+
+    [[nodiscard]] common::result::Result<std::vector<domain::Medication>>
+    getAllMedications() override;
+
+    [[nodiscard]] common::result::Result<domain::Medication> findMedicationById(
+        int medication_id) override;
+
+    [[nodiscard]] common::result::Result<void> deleteMedicationById(int medication_id) override;
+
+    [[nodiscard]] common::result::Result<void> updateMedicationName(
+        int medication_id, std::string_view name) override;
+
+    [[nodiscard]] common::result::Result<void> updateMedicationStrength(
+        int medication_id, std::string_view new_strength) override;
+
+    [[nodiscard]] common::result::Result<void> updateMedicationWarnings(
+        int medication_id, std::string_view new_warnings) override;
+
+  private:
+    infrastructure::db::Database& db_;
+};
+} // namespace infrastructure::persistence::sqlite
