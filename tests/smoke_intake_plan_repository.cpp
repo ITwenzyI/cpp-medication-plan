3357be5558e607147245e258f7a04ca0703e08ff
Kilian <kischoenhaar@gmail.com>
2026-02-27 20:43:47 +0100

    test(intake_plan): add new test block for get intake_plan
    - test isOK and size == 1
    - test all member of intake_plan (id, patient_id ...)
    - test dose with string and the value of created
    - test time_of_day with string and domain::TimeOfDay::Noon
    - test notes with string and the value of created

--- tests/smoke_intake_plan_repository.cpp
+++ tests/smoke_intake_plan_repository.cpp
@@ -33,17 +33,45 @@ int main() {
 
     // Create Patient
     auto created_patient = repo_patient.createPatient(p1);
+    expect(created_patient.isOk(), "created intake_plan should succeed");
     expect(created_patient.value().id > 0, "created patient should have generated id");
 
     // Create Medication
     auto created_med = repo_med.createMedication(m1);
+    expect(created_med.isOk(), "created medication should succeed");
     expect(created_med.value().id > 0, "created medication should have generated id");
 
     // ======= CREATE INTAKE_PLAN ======
     domain::IntakePlan plan1{0, created_patient.value().id, created_med.value().id, "400mg",
         domain::TimeOfDay::Noon, "Take with food!"};
     auto created_plan = repo_plan.createIntakePlan(plan1);
+    expect(created_plan.isOk(), "created intake_plan should succeed");
     expect(created_plan.value().id > 0, "created intake_plan should have generated id");
+
+    // ======= GET INTAKE_PLAN BY PATIENT_ID ======
+    auto plans_by_patient_id = repo_plan.getIntakePlansByPatientId(created_patient.value().id);
+    expect(plans_by_patient_id.isOk(), "get intake_plans by patient_id should succeed");
+    expect(plans_by_patient_id.value().size() == 1,
+        "expected exactly one intake_plan in test database");
+    expect(plans_by_patient_id.value()[0].id == created_plan.value().id, "id should match");
+    expect(plans_by_patient_id.value()[0].patientId == created_plan.value().patientId,
+        "patient_id should match");
+    expect(plans_by_patient_id.value()[0].medicationId == created_plan.value().medicationId,
+        "medication_id should match");
+
+    expect(plans_by_patient_id.value()[0].dose == created_plan.value().dose,
+        "dose should match with created");
+    expect(plans_by_patient_id.value()[0].dose == "400mg", "dose should match with string");
+
+    expect(plans_by_patient_id.value()[0].timeOfDay == created_plan.value().timeOfDay,
+        "time_of_day should match with created");
+    expect(plans_by_patient_id.value()[0].timeOfDay == domain::TimeOfDay::Noon,
+        "time_of_day should match with string");
+
+    expect(plans_by_patient_id.value()[0].notes == created_plan.value().notes,
+        "notes should match with created");
+    expect(plans_by_patient_id.value()[0].notes == "Take with food!",
+        "notes should match with string");
 
     std::cout << "INTAKE_PLAN SMOKE TEST PASSED\n";
 
