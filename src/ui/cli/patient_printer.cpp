5b25c117fe71265ea19c71829bf7ce7daf50d7d9
Kilian <kischoenhaar@gmail.com>
2026-03-01 13:29:38 +0100

    refactor(patient_printer): update to new nationality enum class

--- src/ui/cli/patient_printer.cpp
+++ src/ui/cli/patient_printer.cpp
@@ -1,4 +1,5 @@
 #include "patient_printer.hpp"
+#include "infrastructure/persistence/sqlite/nationality_mapper_sqlite.hpp"
 #include <iostream>
 
 namespace ui::cli {
@@ -11,9 +12,13 @@ static std::string formatField(const std::string& field) {
 }
 
 void printPatient(const domain::Patient& p) {
+    const std::string nationality = p.nationality.has_value()
+        ? infrastructure::persistence::sqlite::nationalityToDbString(p.nationality.value())
+        : "";
+
     std::cout << "ID: " << p.id << " Name: " << formatField(p.name)
               << " BirthDate: " << formatField(p.birth_date)
-              << " Nationality: " << formatField(p.nationality) << "\n";
+              << " Nationality: " << formatField(nationality) << "\n";
 }
 
 void printPatients(const std::vector<domain::Patient>& patients) {
