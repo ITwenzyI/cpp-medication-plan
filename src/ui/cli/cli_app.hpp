d5d324910c099a5dfebd2b50b7afa87e773a5779
Kilian <kischoenhaar@gmail.com>
2026-03-04 11:44:51 +0100

    feat(ui): add cli_app header file
    - include only public constructor and the main function run
    - private function blocks for every category like menu, patient ...
    - simple utility functions
    - one printError Message function, will be replaced later

---
+++ src/ui/cli/cli_app.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "common/result/result.hpp"
+#include "infrastructure/db/database.hpp"
+#include "infrastructure/persistence/sqlite/intake_plan_repository_sqlite.hpp"
+#include "infrastructure/persistence/sqlite/medication_repository_sqlite.hpp"
+#include "infrastructure/persistence/sqlite/patient_repository_sqlite.hpp"
+
+namespace ui::cli {
+
+class CliApp {
+  public:
+    explicit CliApp(infrastructure::db::Database& db);
+
+    [[nodiscard]] int run();
+
+  private:
+    infrastructure::db::Database& db_;
+    infrastructure::persistence::sqlite::PatientRepositorySqlite patientRepo_;
+    infrastructure::persistence::sqlite::MedicationRepositorySqlite medicationRepo_;
+    infrastructure::persistence::sqlite::IntakePlanRepositorySqlite intakePlanRepo_;
+
+    // Menu
+    void mainMenuLoop();
+    void showMainMenu() const;
+    void patientsMenuLoop();
+    void showPatientsMenu() const;
+    void medicationsMenuLoop();
+    void showMedicationsMenu() const;
+    void intakePlansMenuLoop();
+    void showIntakePlansMenu() const;
+
+    // Patient Commands
+    void cmdListPatients();
+    void cmdCreatePatient();
+    void cmdFindPatientById();
+
+    // Medications Commands
+    void cmdListMedications();
+    void cmdCreateMedication();
+
+    // IntakePlans Commands
+    void cmdListIntakePlans();
+    void cmdCreateIntakePlan();
+
+    // Utility
+    void waitForEnter() const;
+    common::result::Result<int> readInt(std::string_view prompt) const;
+
+    // Error Message
+    void printError(const common::result::AppError&) const;
+};
+
+} // namespace ui::cli
