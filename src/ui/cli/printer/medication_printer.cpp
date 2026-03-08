698a58dd71d04df8558cedc64deb5240b4d0ca5e
Kilian <kischoenhaar@gmail.com>
2026-03-08 13:31:57 +0100

    feat(ui::cli::printer): add printMedicationTableHeader

--- src/ui/cli/printer/medication_printer.cpp
+++ src/ui/cli/printer/medication_printer.cpp
@@ -15,15 +15,30 @@ std::string formatField(const std::string& field) {
 
 constexpr int titleWidth = 25;
 constexpr int idWidth = 8;
-constexpr int nameWidth = 18;
+constexpr int nameWidth = 15;
 constexpr int strengthWidth = 14;
 constexpr int warningsWidth = 25;
-constexpr int seperation_line = 60;
+constexpr int seperation_line = 64;
 } // namespace
 
 namespace ui::cli::printer {
 
 void printMedicationTableHeader() {
+    for (int i = 0; i < titleWidth; i++) {
+        std::cout << "=";
+    }
+    std::cout << " Medications ";
+    for (int i = 0; i < titleWidth; i++) {
+        std::cout << "=";
+    }
+    std::cout << "\n\n";
+    std::cout << std::left << std::setw(idWidth) << "ID" << std::setw(nameWidth) << "Name"
+              << std::setw(strengthWidth) << "Strength" << std::setw(warningsWidth) << "Warnings"
+              << "\n";
+    for (int i = 0; i < seperation_line; i++) {
+        std::cout << "-";
+    }
+    std::cout << "\n";
 }
 
 void printMedicationRow(const domain::Medication& medication) {
